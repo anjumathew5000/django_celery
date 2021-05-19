6b645ed30bb9269fe665b6fb0b94afec04fb0c3a
anju1511 <anjumathew1511@gmail.com>
2021-05-19 10:30:45 -0700

    redme

--- example/views.py
+++ example/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .task import *
 from .helper import *
 
-# Create your views here.
+# Create your views.
 def index(request):
     send_mail_task.delay(300)
   
