cfde5e01142fe07c8c0d911e4c85ed06eb07eedf
anjumathew5000 <77531573+anjumathew5000@users.noreply.github.com>
2021-05-19 10:33:08 -0700

    Update helper.py

--- example/helper.py
+++ example/helper.py
@@ -2,5 +2,5 @@ from django.core.mail import send_mail
 
 
 def send_email_without_celery():
-    send_mail('CELERYWORKED',"CELERY IS COOL",'anju.m@irohub.com',['anjumathew1511@gmail.com'],fail_silently = False)
+    send_mail('CELERYWORKED',"CELERY IS cool",'anju.m@irohub.com',['anjumathew1511@gmail.com'],fail_silently = False)
     return None
